a31b2f335a17f1b5b314f3d13fad03fdd0e67a05
Itonsow <gustavoito223@gmail.com>
2023-10-15 16:40:21 -0300

    1.0

--- database/migrations/2023_06_26_160451_create_reply_supports_table.php
+++ database/migrations/2023_06_26_160451_create_reply_supports_table.php
@@ -11,20 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('replies_support', function (Blueprint $table) {
-            $table->uuid('id')->primary();
-            $table->uuid('user_id')->index();
-            $table->uuid('support_id')->index();
-            $table->text('content');
+        Schema::create('supports', function (Blueprint $table) {
+            $table->id();
+            $table->string('subject');
+            $table->enum('status', ['a', 'p', 'c']);
+            $table->text('body');
             $table->timestamps();
-
-            $table->foreign('user_id')
-                ->references('id')
-                ->on('users');
-
-            $table->foreign('support_id')
-                ->references('id')
-                ->on('supports');
         });
     }
 
